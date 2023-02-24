186ad67cd34f5cc655e3802987350ee75fa417ba
Kaho Fujita <105876236+kahofujita@users.noreply.github.com>
2023-02-23 16:19:51 -0800

    implemented select function

--- client/src/components/forms/AddCar.js
+++ client/src/components/forms/AddCar.js
@@ -6,17 +6,35 @@ import FormItem from "antd/es/form/FormItem";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useMutation } from "@apollo/client";
-import { ADD_CAR, GET_CARS } from "../../queries";
+import { ADD_CAR, GET_CARS, GET_PEOPLE } from "../../queries";
+import { Select } from "antd";
+import { useQuery } from "@apollo/client";
 
 const AddCar = () => {
   const [id] = useState(uuidv4());
   const [addCar] = useMutation(ADD_CAR);
   const [form] = Form.useForm();
   const [, forceUpdate] = useState();
+  const [, setValue] = useState("");
 
   useEffect(() => {
     forceUpdate([]);
   }, []);
+
+  const selectOptionArray = [];
+
+  const { loading, error, data } = useQuery(GET_PEOPLE);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  data.people.map(({ id, firstName, lastName }) =>
+    selectOptionArray.push({ value: id, label: `${firstName} ${lastName}` })
+  );
+
+  const onChange = (value) => {
+    setValue(value);
+  };
 
   const onFinish = (values) => {
     const { year, make, model, price, personId } = values;
@@ -83,9 +101,18 @@ const AddCar = () => {
       <FormItem
         label="Person: "
         name="personId"
-        rules={[{ required: true, message: "Please input person!" }]}
+        rules={[{ required: true, message: "Please select person!" }]}
       >
-        <Input placeholder="Person" />
+        <Select
+          showSearch
+          placeholder="Select a person"
+          optionFilterProp="children"
+          onChange={onChange}
+          filterOption={(input, option) =>
+            (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+          }
+          options={selectOptionArray}
+        />
       </FormItem>
       <FormItem shouldUpdate={true}>
         {() => (
