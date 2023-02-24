ecfb47887e933c8f86954ba7ebf4d0e5ff805b09
Kaho Fujita <105876236+kahofujita@users.noreply.github.com>
2023-02-24 11:53:52 -0800

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import HomeScreen from "./components/screens/HomeScreen";
-import PersonCard from "./components/listitems/PersonCard";
 import ShowScreen from "./components/screens/ShowScreen";
 
 const client = new ApolloClient({
